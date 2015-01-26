a3a0833a8346bc7096ec67208adecba49b565638
Robert Bragg <robert@sixbynine.org>
2015-01-26 16:11:26 +0000

    intel: Add a getter for the intel_context ctx_id
    Exposing the context-id makes it possible to define non-drm kernel
    interfaces that can uniquely identify a gem context via a
    <drm_fd,ctx_id> tuple.
    
    For example this can be used to configure a perf event to collect
    metrics for a specific context.
    
    Signed-off: Robert Bragg <robert@sixbynine.org>

--- intel/intel_bufmgr_gem.c
+++ intel/intel_bufmgr_gem.c
@@ -3220,6 +3220,15 @@ drm_intel_gem_context_create(drm_intel_bufmgr *bufmgr)
 	return context;
 }
 
+drm_public uint32_t
+drm_intel_gem_context_get_context_id(drm_intel_context *ctx)
+{
+	if (ctx == NULL)
+		return -EINVAL;
+
+	return ctx->ctx_id;
+}
+
 drm_public void
 drm_intel_gem_context_destroy(drm_intel_context *ctx)
 {
